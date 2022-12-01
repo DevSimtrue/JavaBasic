01329ce3f9f03b6b86588987779491a286976f47
DevSimtrue <simtrue@naver.com>
2022-12-01 10:44:45 +0900

    숫자와 문자열 출력 방법

---
+++ src/Print.java
@@ -0,0 +1,15 @@
+public class Print {
+    public static void main(String[] args) {
+        //숫자 출력 및 연산 가능
+        System.out.println(1+3.5);
+
+        //문자열과 문자의 차이 "", ''의 차이 발생
+        System.out.println("A"+3);
+        System.out.println('A'+3);
+
+        //문자열 출력 및 편집 방법
+        System.out.println("나는" + " \"자바\"야.");
+        System.out.println("나는" + "\n자바야.");
+
+    }
+}
